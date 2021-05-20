6f0bdc96ceef1c0242bde5896d37fca09ea99044
bemuse01 <sequeland@gmail.com>
2021-05-21 04:58:08 +0900

    Update visualizer.child.param.js

--- assets/js/application/visualizer/child2/visualizer.child.param.js
+++ assets/js/application/visualizer/child2/visualizer.child.param.js
@@ -1,7 +1,7 @@
 import AUDIO_PARAM from '../../audio/audio.param.js'
 
 export default {
-    width: 750,
+    width: 500,
     height: 5,
     seg: AUDIO_PARAM.display - 1,
     size: AUDIO_PARAM.size,
